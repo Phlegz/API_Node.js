1a47b4ec266001f335b4bb32284671b7ea4aeced
Stephen Brown <stembro@Stephens-MacBook-Air.local>
2017-03-21 21:16:31 -0600

    simple get and post handlers

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
-})
+});
 
 app.post('/',function(req, res){
-  console.log(req);
-})
+  req.on('data', (data) => {
+    console.log(data.toString('utf8'));
+  });
+  res.status(204).end();
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
