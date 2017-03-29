d4290bb18dd6dffcd0b2dbf145e4e7d8d9f94151
farnaz <farnaz.rashidi@gmail.com>
2017-03-28 20:28:28 -0700

    Implement a test for the endpoint that returns an event by id

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const 
+const
     chai = require('chai'),
     chaiHttp = require('chai-http'),
     servicePath = 'http://localhost:3000';
@@ -18,5 +18,29 @@ describe('API', function() {
                 throw err;
             });
     });
-});
+    it('returns a single event if id is valid', function() {
+        return chai.request(servicePath)
+            .get('/api/events/3')
+            .then(function (res) {
+                chai.expect(res).to.have.status(200);
+            })
+            .catch(function (err) {
+                throw err;
+            });
+    });
+    // it('returns a single event if id is not valid', function() {
+    //     return chai.request(servicePath)
+    //         .get('/api/events/3')
+    //         .then(function (res) {
+    //
+    //           chai.expect(res).to.have.status(404)
+    //
+    //         })
+    //         .catch(function (err) {
+    //           console.log(err);
+    //           chai.expect(true).to.be.true
+    //           console.log(err.response.statusCode);
+    //         });
+    // });
 
+});
